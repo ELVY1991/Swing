8c99145264d3c6117921f9639644e92764bb0cdd
ELVY1991 <596475962@qq.com>
2016-05-30 00:25:42 +0800

    home

---
+++ MonkeyTool/src/Monkey/Frame.java
@@ -0,0 +1,105 @@
+package Monkey;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
+import javax.swing.JTable;
+
+public class Frame extends JFrame {
+
+	private JPanel contentPane;
+	public static JTextArea textArea;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame frame = new Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 656, 382);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		
+		JButton btnNewButton = new JButton("Run");
+		btnNewButton.setBounds(120, 12, 81, 23);
+		contentPane.add(btnNewButton);
+		
+		JComboBox comboBox = new JComboBox(Util.getDeviceId());
+		comboBox.setBounds(10, 13, 92, 21);
+		comboBox.setSelectedItem(0);
+		contentPane.add(comboBox);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(211, 11, 362, 243);
+		contentPane.add(scrollPane);
+		
+		textArea = new JTextArea();
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
+		
+		JComboBox comboBox_package = new JComboBox(Util.listPackages());
+		comboBox_package.setBounds(211, 265, 153, 23);
+		comboBox_package.setSelectedItem(0);
+		contentPane.add(comboBox_package);
+		
+		JComboBox comboBox_cmd = new JComboBox(Util.cmdList());
+		comboBox_cmd.setBounds(10, 44, 92, 21);
+		contentPane.add(comboBox_cmd);
+		
+
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String device = null;
+				String command = null;
+				if ( comboBox.getSelectedIndex()!= -1) {
+					 device = (String) comboBox.getItemAt(comboBox.getSelectedIndex());
+				}
+				if ( comboBox_cmd.getSelectedIndex()!= -1) {
+					 command = (String) comboBox_cmd.getItemAt(comboBox_cmd.getSelectedIndex());
+				}
+				Util.doCommand(device,command);
+			}
+		});
+	}
+}
---
+++ MonkeyTool/src/Monkey/Util.java
@@ -0,0 +1,113 @@
+package Monkey;
+
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
+import javax.swing.DefaultComboBoxModel;
+import org.eclipse.ui.internal.dialogs.AdaptableForwarder;
+
+public class Util {
+
+	public static DefaultComboBoxModel<String> getDeviceId(){
+		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
+		
+		try {
+			Process p = Runtime.getRuntime().exec("cmd /c adb devices");
+			InputStream inputStream = p.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			String line;
+			while ((line= bufferedReader.readLine())!= null) {
+				if (line.endsWith("device")) {
+					String[] list =line.split("\t");
+					model.addElement(list[0]);
+					System.out.println(list[0]);
+				}
+			}
+			bufferedReader.close();
+			inputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return model;
+		
+	}	 
+		
+	public static ArrayList<String> doCommand(String deviceId ,String command){
+
+		ArrayList<String> list = new ArrayList<>();
+		try {
+			Process p = Runtime.getRuntime().exec("cmd /c adb -s "+ deviceId + " "+command);
+			InputStream inputStream = p.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			String line;
+			while ((line= bufferedReader.readLine())!= null) {
+				list.add(line);
+				Frame.textArea.append(line);
+			}
+			bufferedReader.close();
+			inputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	
+	public static DefaultComboBoxModel<String> listPackages() {
+		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
+		try {
+			Process p = Runtime.getRuntime().exec("cmd /c adb shell pm list packages");
+			InputStream inputStream = p.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			String line;
+			while ((line= bufferedReader.readLine())!= null) {
+				if (line.startsWith("package")) {
+					String[] strings = line.split(":");
+					model.addElement(strings[1]);
+					System.out.println(line);
+				}
+					
+				}
+			bufferedReader.close();
+			inputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return model;
+	}
+	
+	public static DefaultComboBoxModel<String> cmdList() {
+		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
+		HashMap<String, String> map = new HashMap<>();
+		map.put("reboot", "reboot");
+		map.put("Focued Activity", "shell dumpsys activity | findstr \"mF\"");
+		Set set = map.keySet();
+		Iterator iterator = set.iterator();
+		while (iterator.hasNext()) {
+			String string = (String) iterator.next();
+			model.addElement(string);
+		}
+		return model;
+		
+	}
+
+	public static void main(String[] args) {
+		getDeviceId();
+//		doCommand("8fe9d6f3","reboot");
+		cmdList();
+	}
+}
